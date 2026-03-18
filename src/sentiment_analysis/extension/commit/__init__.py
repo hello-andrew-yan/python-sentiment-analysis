19a4fef077691af5ed5bfbee73e85ca0f5a4583a
Andrew Yan <hello.andrew.yan@outlook.com>
2026-03-18 16:12:17 +1100

    feat: Extracted mask loop logic.

---
+++ src/sentiment_analysis/extension/commit/__init__.py
@@ -0,0 +1,3 @@
+from .filters import Contains, Length
+
+__all__ = ["Contains", "Length"]
--- src/sentiment_analysis/main.py
+++ src/sentiment_analysis/main.py
@@ -4,27 +4,35 @@ import polars as pl
 from tqdm import tqdm
 
 from sentiment_analysis.core.filter import Filter
-from sentiment_analysis.extension.commit_filters import Contains, Length
+from sentiment_analysis.extension.commit import Length
 
-if __name__ == "__main__":
-    base_dir = Path(__file__).resolve().parents[2]
-    data_dir = base_dir / "data"
 
-    csv_path = data_dir / "csv" / "dhruvildave_github-commit-messages-dataset.csv"
-    target_col = "message"
-
-    filters: list[Filter] = [
-        Length(target_col, 0, 14),
-        ~Length(target_col, 0, 14),
-        Length(target_col, 15, 72),
-        Contains(target_col, "test"),
-    ]
-
+def apply_masks(
+    csv_path: Path,
+    output_dir: Path,
+    filters: list[Filter],
+    export_cols: list[str] | str,
+) -> None:
     for i, f in enumerate(tqdm(filters, desc="Processing masks"), start=1):
-        export_path = data_dir / "csv" / f"mask_{i}_{f.name}.csv"
+        export_path = output_dir / f"mask_{i}_{f.name}.csv"
         (
             pl.scan_csv(csv_path, infer_schema_length=0, encoding="utf8")
             .filter(f())
-            .select(target_col)
+            .select(export_cols)
             .sink_csv(export_path)
         )
+
+
+if __name__ == "__main__":
+    base_dir = Path(__file__).resolve().parents[2]
+    csv_dir = base_dir / "data" / "csv"
+    csv_dir.mkdir(parents=True, exist_ok=True)
+
+    csv_path = csv_dir / "dhruvildave_github-commit-messages-dataset.csv"
+    target_col = "message"
+    filters: list[Filter] = [
+        Length(target_col, 0, 9),
+        Length(target_col, 10, 72),
+    ]
+
+    apply_masks(csv_path, output_dir=csv_dir, filters=filters, export_cols=target_col)
